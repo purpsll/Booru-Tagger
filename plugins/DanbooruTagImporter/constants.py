dff278130ed8d0fef52400b7edbd53a9f0c1abfb
purpsl <105087296+purpsll@users.noreply.github.com>
2026-09-17 21:09:57 -0500

    Bump Booru Importer to 3.26.9

--- plugins/DanbooruTagImporter/constants.py
+++ plugins/DanbooruTagImporter/constants.py
@@ -4,7 +4,7 @@ Credentials / account identifiers plus the SauceNAO polling ceiling are user-con
 in Stash. Other operational presets live here so installations share the same policy.
 """
 
-VERSION = "3.26.8"
+VERSION = "3.26.9"
 PLUGIN_ID = "DanbooruTagImporter"
 USER_AGENT = f"stash-multibooru-tag-importer/{VERSION}"
 
