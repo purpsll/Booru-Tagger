d572b66b85b7706b8932750f3a6a46f91f6ec444
purpsl <105087296+purpsll@users.noreply.github.com>
2026-09-21 15:42:24 -0500

    Preserve full entity state after update mutations

--- plugins/StashMetadataMigrator/stash_client.py
+++ plugins/StashMetadataMigrator/stash_client.py
@@ -273,9 +273,13 @@ class Stash:
         query = """
         mutation MigratorUpdateTag($input: TagUpdateInput!) {
           tagUpdate(input: $input) {
-            id name aliases sort_name description favorite ignore_auto_tag
+            id name aliases sort_name description favorite ignore_auto_tag image_path
             stash_ids { endpoint stash_id }
+            parents { id name }
+            children { id name }
             custom_fields
+            scene_count scene_marker_count image_count gallery_count
+            performer_count studio_count group_count
           }
         }
         """
@@ -311,10 +315,11 @@ class Stash:
             id name disambiguation alias_list urls gender birthdate ethnicity country
             eye_color height_cm measurements fake_tits penis_length circumcised
             career_start career_end tattoos piercings favorite rating100 details
-            death_date hair_color weight ignore_auto_tag
+            death_date hair_color weight ignore_auto_tag image_path
             stash_ids { endpoint stash_id }
             tags { id name }
             custom_fields
+            scene_count image_count gallery_count group_count
           }
         }
         """
@@ -347,10 +352,14 @@ class Stash:
         query = """
         mutation MigratorUpdateStudio($input: StudioUpdateInput!) {
           studioUpdate(input: $input) {
-            id name aliases urls rating100 favorite details ignore_auto_tag organized
+            id name aliases urls rating100 favorite details ignore_auto_tag organized image_path
+            parent_studio { id name }
+            child_studios { id name }
             stash_ids { endpoint stash_id }
             tags { id name }
             custom_fields
+            scene_count(depth: 0) image_count(depth: 0)
+            gallery_count(depth: 0) group_count(depth: 0)
           }
         }
         """
